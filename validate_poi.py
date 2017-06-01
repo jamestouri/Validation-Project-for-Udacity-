4a80c5a4f08562126330c91052a8c4c20c65368f
James Touri <jtouri@trademeer.com>
2017-05-31 23:19:13 -0700

    Validation Project for Udacity
    Udacity’s machine learning project for validation

---
+++ validate_poi.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+
+
+"""
+    Starter code for the validation mini-project.
+    The first step toward building your POI identifier!
+
+    Start by loading/formatting the data
+
+    After that, it's not our code anymore--it's yours!
+"""
+
+import pickle
+import sys
+sys.path.append("../tools/")
+from feature_format import featureFormat, targetFeatureSplit
+
+data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
+
+### first element is our labels, any added elements are predictor
+### features. Keep this the same for the mini-project, but you'll
+### have a different feature list when you do the final project.
+features_list = ["poi", "salary"]
+
+data = featureFormat(data_dict, features_list)
+labels, features = targetFeatureSplit(data)
+
+from sklearn.model_selection import train_test_split
+
+
+### it's all yours from here forward!  
+
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3, random_state=42)
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+
+
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test, labels_test)
+
+#Finding the number of poi
+the_poi = 0
+
+for i in labels_test:
+    if i == 1.0:
+        the_poi += 1
+
+the_poi
+
+#number of total people
+
+len(labels_test)
+
+true_positives = 0
+false_positives = 0
+false_negatives = 0
+
+for predicted, actual in zip(pred, labels_test):
+    if predicted == 1 and actual == 1:
+        true_positives += 1
+    elif predicted == 1 and actual == 0:
+        false_positives += 1
+    elif predicted == 0 and actual == 1:
+        false_negatives += 1
+
+
+
+print(true_positives)
+print(false_negatives)
+print(false_positives)
+
+from sklearn.metrics import precision_score
+
+print(precision_score(pred, labels_test))
+
+from sklearn.metrics import recall_score
+print(recall_score(pred, labels_test))
+
